dfb9eb17ba78d16c8c8ec5acdc69b8e2803b135f
Felix Freidman <felixfreidman@gmail.com>
2022-03-02 17:10:54 +0800

    5:10PM Commit New Swiper for buyout

--- src/assets/js/components/components.js
+++ src/assets/js/components/components.js
@@ -1,27 +1,30 @@
-// // header-swiper
-// var swiper = new Swiper('#main-swiper', {
-//   fadeEffect: {
-//     crossFade: true
-//   },
-//   navigation: {
-//     nextEl: '.swiper-button-next--header',
-//     prevEl: '.swiper-button-prev--header',
-//   },
-//   pagination: {
-//     el: '.swiper-pagination',
-//     type: 'bullets',
-//     clickable: true,
-//   },
-//   loop: true,
-//   // autoplay: {
-//   //   delay: 2300,
-//   // },
-//   fadeEffect: {
-//     crossFade: true
-//   },
-//   speed: 800,
-//   watchSlidesProgress: true,
-//   watchVisibility: true,
-//   disableOnInteraction: true,
-// });
-
+// header-swiper
+var swiper = new Swiper("#buyoutSwiper", {
+    fadeEffect: "cards",
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev ",
+    },
+    loop: true,
+    speed: 800,
+    slidesPerView: 3,
+    spaceBetween: 50,
+    // Responsive breakpoints
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+        },
+        // when window width is >= 480px
+        420: {
+            slidesPerView: 2,
+            spaceBetween: 30,
+        },
+        // when window width is >= 640px
+        700: {
+            slidesPerView: 3,
+            spaceBetween: 40,
+        },
+    },
+});
